0ecf44405b9d55a95c766231757ef1e7e0e53dae
Zven94 <nicobolanez@gmail.com>
2023-07-05 11:54:23 -0300

    Fix linters issues

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import ShowButtons from './components/Calculator';
 function App() {
   return (
     <main className="calc-divs">
-      <div className='row-total'>
+      <div className="row-total">
         <h2 className="calc-total">0</h2>
       </div>
 
-      <div className='calc-grid'>
+      <div className="calc-grid">
         <ShowButtons />
       </div>
     </main>
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,26 +3,24 @@ import React from 'react';
 const calcButtons = ['AC', '+/-', '%', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
 function ShowButtons() {
-
   function CreateElement(text, index) {
     let className = '';
-    let divClassName = 'row-one';
     if ((index + 1) % 4 === 0) {
-      className = ' makeMe-orange'
-    } else if ( {text}.text === '0') {
-      className = ' calc-button-0'
+      className = ' makeMe-orange';
+    } else if ({ text }.text === '0') {
+      className = ' calc-button-0';
     }
 
     return (
-      
-      <button key={text} className={className}>{text}</button>
-      
+
+      <button key={text} className={className} type="button">{text}</button>
+
     );
   }
 
   return (
     calcButtons.map(CreateElement)
   );
-};
+}
 
 export default ShowButtons;
