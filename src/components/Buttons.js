02fdfe1c06b32b37d5cd92f4e525cc56002b2833
Zven94 <nicobolanez@gmail.com>
2023-07-05 16:20:29 -0300

    Create a child for my Calculator.js

---
+++ src/components/Buttons.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const calcButtons = ['AC', '+/-', '%', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
+function Buttons() {
+  function CreateElement(text, index) {
+    let className = '';
+    if ((index + 1) % 4 === 0 || (index + 1) === 19) {
+      className = ' makeMe-orange';
+    } else if ({ text }.text === '0') {
+      className = ' calc-button-0';
+    }
+
+    return (
+
+      <button key={text} className={className} type="button">{text}</button>
+
+    );
+  }
+
+  return (
+    calcButtons.map(CreateElement)
+  );
+}
+
+export default Buttons;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,25 +1,9 @@
 import React from 'react';
-
-const calcButtons = ['AC', '+/-', '%', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+import Buttons from './Buttons';
 
 function Calculator() {
-  function CreateElement(text, index) {
-    let className = '';
-    if ((index + 1) % 4 === 0 || (index + 1) === 19) {
-      className = ' makeMe-orange';
-    } else if ({ text }.text === '0') {
-      className = ' calc-button-0';
-    }
-
-    return (
-
-      <button key={text} className={className} type="button">{text}</button>
-
-    );
-  }
-
   return (
-    calcButtons.map(CreateElement)
+    <Buttons />
   );
 }
 
