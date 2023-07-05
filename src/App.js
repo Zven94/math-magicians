22c0454b3e1661e57fc1960500c7bd9388ea462c
Zven94 <nicobolanez@gmail.com>
2023-07-05 17:19:58 -0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
 function App() {
+
+  const [pressedButton, setPressedButton] = useState('0');
+
+  const handleButtonClicked = (button) => {
+    setPressedButton(button)
+  }
+
   return (
     <main className="calc-divs">
       <div className="row-total">
-        <h2 className="calc-total">0</h2>
+        <h2 className="calc-total">{pressedButton}</h2>
       </div>
 
       <div className="calc-grid">
-        <Calculator />
+        <Calculator handleButtonClicked={handleButtonClicked}/>
+        <button onClick={() => handleButtonClicked('1')}>1</button>
       </div>
     </main>
   );
