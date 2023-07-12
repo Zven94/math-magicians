8abb097e7cfd7cc28d2b0ccf9610dcbe46d31422
Nico Emiliano <nicobolanez@gmail.com>
2023-07-12 13:21:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
